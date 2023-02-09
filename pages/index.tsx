baf450fcdc02a1787ade3b2cd7fab967149477a7
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-02-09 13:23:04 -0300

    test

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 text-red-700 font-bold">
+      Hello World
+    </div>
+  )
+}
+
+export default Home
